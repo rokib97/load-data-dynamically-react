897ce4f92e2ef0edbd970573c86dbcc2e6e72738
rokib97 <hasanrokib1997@gmail.com>
2022-03-16 18:55:57 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App mt-5">
+      <LoadPhone></LoadPhone>
     </div>
   );
 }
 
+const LoadPhone = () => {
+  const [phones, setPhones] = useState([]);
+  useEffect(() => {
+    fetch("https://openapi.programming-hero.com/api/phones?search=samsung")
+      .then((res) => res.json())
+      .then((data) => setPhones(data.data));
+  }, []);
+  return (
+    <div>
+      <div className="phones">
+        {phones.map((phone) => (
+          <Showphone
+            brand={phone.brand}
+            phoneName={phone.phone_name}
+            image={phone.image}
+          ></Showphone>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+const Showphone = (props) => {
+  return (
+    <div className="details">
+      <img src={props.image} alt="" />
+      <h3>Brand: {props.brand}</h3>
+      <h4>Phone Name: {props.phoneName}</h4>
+    </div>
+  );
+};
 export default App;
